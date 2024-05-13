70f102424db1526582a9790cb28c265f3748aeaa
FedeFus <fusarofede.15@gmail.com>
2024-05-13 19:25:32 +0200

    add: form eventlistner with price calculation to main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,21 @@ const calcForm = document.getElementById('calc-form');
 const passengerAge = document.getElementById('passenger-age');
 const passengerKm = document.getElementById('passenger-km');
 
+// form eventlistner
+calcForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let price = 0.21 * passengerKm.value;
+
+    // possibile sconto in base all'età
+    if (passengerAge.value < 18) {
+        price = price * 0.8;      //sconto del 20%
+    } else if (passengerAge.value > 65) {
+        price = price * 0.6;      //sconto del 40%
+    }
+
+    // prezzo arrotondato
+    price = Math.round(price * 100) / 100;
+    console.log(price);
+
+});
