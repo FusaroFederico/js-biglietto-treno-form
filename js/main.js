77df39a64150fd404250dea1e53af2c27aeaafdd
FedeFus <fusarofede.15@gmail.com>
2024-05-13 19:24:03 +0200

    add: variable inizialization to main.js and form id to index.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 // strict mode
 ('use strict');
 
+// inizializzazione delle variabili
+const calcForm = document.getElementById('calc-form');
+const passengerAge = document.getElementById('passenger-age');
+const passengerKm = document.getElementById('passenger-km');
+
